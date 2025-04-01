9f4ee62dce2fc4f54efde12eb6119d6497b5061b
MiningCryptoLive <celldr69@gmail.com>
2025-04-01 13:29:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+function yearPlugin() {
+  return {
+    name: "year",
+    transformIndexHtml(html: string): string {
+      return html.replace("__YEAR__", new Date().getFullYear().toString());
+    },
+  } as const;
+}
 export default defineConfig({
   plugins: [react()],
+  yearPlugin(),
   server: {
     proxy: {
       '/api': {
