a450350d5da6288cf7d1524736e1e5319540e599
MiningCryptoLive <celldr69@gmail.com>
2025-04-01 13:38:19 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Copyright &copy; __YEAR__ My Name  All Right Reserved.</p>
+      <p>Copyright &copy; 2025 crypto.solopool.pro All Right Reserved.</p>
     </div>
   )
 }
