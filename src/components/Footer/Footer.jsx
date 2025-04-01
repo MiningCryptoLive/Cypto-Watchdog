686f83b167a616f5020830c82fa091329ed4467b
MiningCryptoLive <celldr69@gmail.com>
2025-04-01 13:26:59 -0400

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,8 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Copyright @ 2020 - <span id="spanYear"></span> All Right Reserved.</p>
+      <p>Copyright @ 2020  All Right Reserved.</p>
     </div>
   )
 }
-<script>
-  $('#spanYear').html(new Date().getFullYear());
-</script>
 export default Footer
